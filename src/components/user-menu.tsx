bc851e6f7b5bb4d95ea1abfd6b51c1decebf842b
Danny Chambers <connect@designtechnologist.co.uk>
2026-03-11 19:13:24 +0000

    fix: type map param in user-menu, generate Prisma client in CI, add id-token permission to claude workflow

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -14,7 +14,7 @@ function getInitials(user: User): string {
   if (user.name) {
     return user.name
       .split(' ')
-      .map((n) => n[0])
+      .map((n: string) => n[0])
       .join('')
       .toUpperCase()
       .slice(0, 2)
